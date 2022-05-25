ff9bf7ced732741312a0cc8f4eba332dbe9b7ee1
Henrique Borges <henrybf10@hotmail.com>
2022-05-25 09:22:26 -0300

    Add jwt auth

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
 use App\Http\Controllers\AccountController;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,9 +16,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+], function ($router) {
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+    Route::get('me', [AuthController::class, 'me']);
 });
 
-Route::apiResource('accounts', AccountController::class);
-Route::apiResource('users', UserController::class);
+Route::middleware('auth')->group(function() {
+    Route::apiResource('accounts', AccountController::class);
+    Route::apiResource('users', UserController::class);
+});
